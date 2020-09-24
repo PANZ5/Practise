0a59da9ff218014ffbcfb92adb5d6373318597f1
PANZ5 <71678708+PANZ5@users.noreply.github.com>
2020-09-24 15:30:29 +1200

    Start coding program

---
+++ App 1/App 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter something: ");
+        }
+    }
+}
