f5ce0b5cf0cd7e4b8efc44946d037caf3f7f1906
Saniya Vahora (1000095246) <VAHOSY2@student.op.ac.nz>
2020-10-13 14:01:29 +1300

    Game 4 removed

--- App 1/App 1/Program.cs
+++ App 1/App 1/Program.cs
@@ -17,7 +17,7 @@ namespace App_1
             Console.WriteLine("1 Game 1");
             Console.WriteLine("2 Game 2");
             Console.WriteLine("3 Game 3");
-            Console.WriteLine("4 Game 4");
+           
             Console.WriteLine("0 Exit Game menu system");
             Console.Write("Enter a number 1~4 to choose a Game: ");
             string choice = Console.ReadLine();
@@ -32,9 +32,7 @@ namespace App_1
                 case "3":
                     Game3();
                     break;
-                case "4":
-                    Game4();
-                    break;
+                
                 default:
                     Console.Clear();
                     Console.WriteLine("Thanks for using this menu system!");
@@ -82,14 +80,6 @@ namespace App_1
             Console.ReadLine();
             choice();
         }
-        public static void Game4()
-        {
-            Console.Clear();
-            Console.WriteLine("This is Game 4");
-            Console.WriteLine("Press enter to continue...");
-            Console.ReadLine();
-            choice();
-
-        }
+        
     }
 }
